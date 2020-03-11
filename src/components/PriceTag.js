75e226073db43348d493130e97c6bd140970e7c6
Philip Schönholzer <phi.sch@hotmail.ch>
2020-03-11 18:37:47 +0100

    Nicer tablet layout and price formatting

--- src/components/PriceTag.js
+++ src/components/PriceTag.js
@@ -1,22 +1,45 @@
 /* eslint-disable react/style-prop-object */
 import React, { useContext } from 'react'
 import styled from 'styled-components'
-import { FormattedNumber, FormattedMessage } from 'gatsby-plugin-intl'
+import {
+  FormattedNumber,
+  FormattedMessage,
+  FormattedNumberParts,
+} from 'gatsby-plugin-intl'
 
 import CurrencyContext from '../contexts/CurrencyContext'
 
-const PriceTag = ({ price }) => {
+const PriceTag = ({ price, className }) => {
   const { currency } = useContext(CurrencyContext)
 
   return (
-    <Price>
-      <FormattedNumber
+    <Price className={className}>
+      <FormattedNumberParts
         value={price}
         style="currency"
         currency={currency}
-        maximumFractionDigits={0}
+        maximumFractionDigits={2}
         minimumFractionDigits={0}
-      />
+      >
+        {parts => {
+          console.log('parts', parts)
+
+          return parts.map(({ type, value }) => {
+            switch (type) {
+              case 'currency':
+                return <small>{value}</small>
+              case 'integer':
+                return <b>{value}</b>
+              case 'decimal':
+                return <small css="font-size: 0.5em">{value}</small>
+              case 'fraction':
+                return <small css="font-size: 0.5em">{value}</small>
+              default:
+                return value
+            }
+          })
+        }}
+      </FormattedNumberParts>
       <PriceUnit>
         <FormattedMessage id="pricing.plans.pro.priceUnit" />
       </PriceUnit>
--- src/components/TabletCount.js
+++ src/components/TabletCount.js
@@ -10,13 +10,116 @@ import BackNext from '../styles/BackNext'
 import Right from '../styles/Right'
 import Left from '../styles/Left'
 
+const tabletRadioButton = (tabletType, setTabletType) => ({
+  id,
+  name,
+  image,
+  price,
+  specs,
+}) => {
+  return (
+    <label
+      css={`
+        position: relative;
+        transition: transform ease-in 50ms;
+
+        svg {
+          fill: transparent;
+          stroke: transparent;
+        }
+
+        &:hover {
+          transform: translateY(-9px);
+        }
+
+        input:checked + div {
+          background-color: ${p => p.theme.primary};
+          color: white;
+
+          svg {
+            stroke: currentColor;
+          }
+
+          div {
+            color: white !important;
+          }
+        }
+      `}
+    >
+      <input
+        css="appearance: none; position: absolute; height: 100%; width: 100%; cursor: pointer;"
+        type="radio"
+        name="type"
+        value={id}
+        id={id}
+        onChange={() => setTabletType(id)}
+        checked={tabletType === id}
+      />
+      <div
+        css={`
+          display: flex;
+          flex-direction: column;
+          height: 100%;
+          border: 1px solid ${p => p.theme.primary};
+          border-radius: 4px;
+          padding: 1em;
+          transition: background-color 200ms, color 200ms;
+
+          div {
+            transition: color 200ms;
+          }
+          svg {
+            align-self: end;
+          }
+        `}
+      >
+        <p css="margin-top: 0">{name}</p>
+        {image && <Img fluid={image.childImageSharp.fluid} />}
+        {specs && (
+          <ul css="padding-left: 1em; ">
+            <li>
+              Grösse <b>{specs.size}</b>
+            </li>
+            <li>
+              Auflösung <b>{specs.resolution}</b>
+            </li>
+          </ul>
+        )}
+        <div css="display: flex; justify-content: space-between; font-size: 0.6em; margin-top: auto;">
+          <PriceTag css="padding: 1em 0 0;" price={price.oneYear} />
+          <svg width="20" height="20" viewBox="-2 -2 20 20">
+            <path
+              d="M0,0L0,16L16,16L16,0Z"
+              strokeWidth="1"
+              strokeLinejoin="round"
+              css={`
+                stroke: ${p => p.theme.primary};
+              `}
+            />
+            <path d="M2,8L6,12L16,0" strokeWidth="3" />
+            <path d="M12,0L0,0L0,16L16,16L16,4" strokeWidth="1" />
+          </svg>
+        </div>
+      </div>
+    </label>
+  )
+}
+
 const query = graphql`
   {
-    items: allMarkdownRemark {
+    items: allMarkdownRemark(sort: { fields: frontmatter___sort, order: ASC }) {
       nodes {
         frontmatter {
+          id
           name
-          price
+          price {
+            oneYear
+            twoYears
+          }
+          specs {
+            size
+            resolution
+          }
           image {
             childImageSharp {
               fluid(maxWidth: 400) {
@@ -34,7 +137,15 @@ export default injectIntl(({ intl }) => {
   const tablets = useStaticQuery(query).items.nodes.map(
     ({ frontmatter }) => frontmatter
   )
+  console.log('tablets', tablets)
+
   const [tabletCount, setTabletCount] = useState(10)
+  const [tabletType, setTabletType] = useState('none')
+
+  const tabletRadioButtonWithState = tabletRadioButton(
+    tabletType,
+    setTabletType
+  )
 
   const maxTablets = 50
 
@@ -65,29 +176,7 @@ export default injectIntl(({ intl }) => {
         }}
         onChange={tabletCountChanged}
       />
-      <h3 css="font-size: 1.4em; margin-top: 4em;">
-        Welches Tablet möchten sie mieten?
-      </h3>
-      <ul css="display: grid; list-style: none; padding: 0; gap: 1em; grid-template-columns: repeat(auto-fill, minmax(140px, 1fr));">
-        {tablets.map(({ name, price, image }) => (
-          <li
-            css={`
-              border: 1px solid ${p => p.theme.primary};
-              border-radius: 4px;
-              padding: 1em;
-            `}
-          >
-            <p css="margin: 0">{name}</p>
-            <Img fluid={image.childImageSharp.fluid} />
-            <p css="margin: 0">{price}</p>
-          </li>
-        ))}
-      </ul>
-      <div
-        css={`
-          margin-top: 3em;
-        `}
-      >
+      <div>
         <h3 css="margin: 4em 0 0;">
           {intl.formatMessage(
             {
@@ -97,6 +186,18 @@ export default injectIntl(({ intl }) => {
           )}
         </h3>
         <PriceTag css="padding: 0 0 3em;" price={totalProPrice(tabletCount)} />
+      </div>
+
+      <h3 css="font-size: 1.4em; ">Welches Tablet möchten sie mieten?</h3>
+      <div css="display: grid; gap: 1em; grid-template-columns: repeat(auto-fill, minmax(160px, 1fr)); margin-bottom: 5em;">
+        {tabletRadioButtonWithState({
+          id: 'none',
+          name: 'Kein Tablet',
+          image: undefined,
+          price: { oneYear: 0, twoYears: 0 },
+          specs: undefined,
+        })}
+        {tablets.map(tabletRadioButtonWithState)}
       </div>
       <BackNext>
         <Right>
