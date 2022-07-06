fcafe4dfd7f0466ecf02726e9b11abf13bc4727d
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2022-07-06 13:55:44 +0200

    Removed old gatsby content

--- old/src/components/prices.js
+++
@@ -1,9 +0,0 @@
-export const menuPrice = 49
-export const orderPrice = 89
-export const licensePrice = 3
-export const takeawayPercentageFee = '2'
-
-export const planPrice = plan =>
-  plan === 'gaston-menu' ? menuPrice : orderPrice
-
-export const totalPrice = (plan) => planPrice(plan)
--- old/src/styles/theme.js
+++
@@ -1,6 +0,0 @@
-const theme = {
-  primary: '#E11387',
-  highlight: '#fac6e340',
-  text: '#4C4C4C',
-}
-export default theme
